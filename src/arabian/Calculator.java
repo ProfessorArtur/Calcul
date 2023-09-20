dca1c25659deb80943e4893b103d044fbe461266
Артур <128334628+ProfessorArtur@users.noreply.github.com>
2023-09-20 18:45:49 +0300

    Update Calculator.java1

--- src/arabian/Calculator.java
+++ src/arabian/Calculator.java
@@ -3,56 +3,62 @@ package arabian;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] input) {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String input = scn.nextLine();
         try {
-            Converter converter = new Converter();
-            String[] actions = {"+", "-", "/", "*"};
-            String[] regexActions = {"\\+", "-", "/", "\\*"};
-            Scanner scn = new Scanner(System.in);
-            String exp = scn.nextLine();
-            int actionIndex = -1;
-            for (int i = 0; i < actions.length; i++) {
-                if (exp.contains(actions[i])) {
-                    actionIndex = i;
-                    break;
-                }
-            }
-            if (actionIndex == -1) {
-                throw new IllegalArgumentException("Некорректное выражение");
-            }
-            String[] data = exp.split(regexActions[actionIndex]);
-            if (data.length != 2) {
-                throw new IllegalArgumentException("В выражении должны быть только два операнда");
-            }
-            if (converter.isRoman(data[0]) == converter.isRoman(data[1])) {
-                int a, b;
-                boolean isRoman = converter.isRoman(data[0]);
-                if (isRoman) {
-                    a = converter.romanToInt(data[0]);
-                    b = converter.romanToInt(data[1]);
-                } else {
-                    a = Integer.parseInt(data[0]);
-                    b = Integer.parseInt(data[1]);
-                }
-                if (a < 1 || a > 10 || b < 1 || b > 10) {
-                    throw new IllegalArgumentException("Числа должны быть от 1 до 10 включительно");
-                }
-                int result = switch (actions[actionIndex]) {
-                    case "+" -> a + b;
-                    case "-" -> a - b;
-                    case "*" -> a * b;
-                    default -> a / b;
-                };
-                if (isRoman) {
-                    System.out.println(converter.intToRoman(result));
-                } else {
-                    System.out.println(result);
-                }
-            } else {
-                throw new IllegalArgumentException("Числа должны быть в одном формате");
-            }
+            String result = calc(input);
+            System.out.println(result);
         } catch (IllegalArgumentException e) {
             System.err.println(e.getMessage());
+        }
+    }
+
+    public static String calc(String input) {
+        Converter converter = new Converter();
+        String[] actions = {"+", "-", "/", "*"};
+        String[] regexActions = {"\\+", "-", "/", "\\*"};
+        String exp = input;
+        int actionIndex = -1;
+        for (int i = 0; i < actions.length; i++) {
+            if (exp.contains(actions[i])) {
+                actionIndex = i;
+                break;
+            }
+        }
+        if (actionIndex == -1) {
+            throw new IllegalArgumentException("Некорректное выражение");
+        }
+        String[] data = exp.split(regexActions[actionIndex]);
+        if (data.length != 2) {
+            throw new IllegalArgumentException("В выражении должны быть только два операнда");
+        }
+        if (converter.isRoman(data[0]) == converter.isRoman(data[1])) {
+            int a, b;
+            boolean isRoman = converter.isRoman(data[0]);
+            if (isRoman) {
+                a = converter.romanToInt(data[0]);
+                b = converter.romanToInt(data[1]);
+            } else {
+                a = Integer.parseInt(data[0]);
+                b = Integer.parseInt(data[1]);
+            }
+            if (a < 1 || a > 10 || b < 1 || b > 10) {
+                throw new IllegalArgumentException("Числа должны быть от 1 до 10 включительно");
+            }
+            int result = switch (actions[actionIndex]) {
+                case "+" -> a + b;
+                case "-" -> a - b;
+                case "*" -> a * b;
+                default -> a / b;
+            };
+            if (isRoman) {
+                return converter.intToRoman(result);
+            } else {
+                return String.valueOf(result);
+            }
+        } else {
+            throw new IllegalArgumentException("Числа должны быть в одном формате");
         }
     }
 }
